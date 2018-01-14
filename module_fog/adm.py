b4d8f9eb41ff898414a7c67c82862eda1e7cd6ce
pachecobeto95 <robertopvc@gmail.com>
2018-01-14 00:20:52 -0200

    Update adm.py

--- module_fog/adm.py
+++ module_fog/adm.py
@@ -7,10 +7,10 @@
 import os
 import sys
 
-params = sys.argv[1]
+params = sys.argv[1] #receive a parameter from the terminal 
 
-os.system("mv ./%s.py manager_fog.py" %(params))
+os.system("mv ./%s.py manager_fog.py" %(params)) #rename the file to parameter name for manager_fog.py. 
 
-os.system("scp ./manager_fog.py pi@192.168.0.1:/home/pi/")
+os.system("scp ./manager_fog.py pi@192.168.0.1:/home/pi/") #send it to fog by ssh protocol
 
-os.system("mv ./manager_fog.py %s.py" %(params))
+os.system("mv ./manager_fog.py %s.py" %(params)) #rename again to the original name
