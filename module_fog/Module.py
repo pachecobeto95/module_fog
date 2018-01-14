8c924f2ec912240a1fe43c3a60fbb37cd11743c6
pachecobeto95 <robertopvc@gmail.com>
2018-01-13 23:32:57 -0200

    Update Module.py

--- module_fog/Module.py
+++ module_fog/Module.py
@@ -1,3 +1,9 @@
+#Authors:Roberto Pacheco
+#Universidade do Estado do Rio de Janeiro
+#Departamento de Eletronica e Telecomunicacoes (DETEL)
+#Project: SensingBus: Remote management of the pre-processing modules
+#Subject: Interface to receive the pre-processing modules by SSH from the network administrator. 
+
 import manager_fog
 class module(object):
 
