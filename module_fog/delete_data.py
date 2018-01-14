3ab4e5ea3367db14da3a6e29b4c85120bc179298
pachecobeto95 <robertopvc@gmail.com>
2018-01-14 00:29:04 -0200

    Update delete_data.py

--- module_fog/delete_data.py
+++ module_fog/delete_data.py
@@ -7,7 +7,7 @@
 def cartridge(tmp): #delete the defective data, do the last line always empty 
 	delete_list = []
 	for i in range(len(tmp)):
-		if (tmp[i][0:-1] == 0):
+		if (tmp[i][0:-1] == 0): #if the last line were empty, add it to delete_list list.
 			delete_list.append(i)
 	for i in reversed(delete_list):
 		del tmp[i]
